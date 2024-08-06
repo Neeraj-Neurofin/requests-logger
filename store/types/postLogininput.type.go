0097d6651a5b80f417a1d179b53ecfb3f00b0bab
Neeraj-Neurofin <neeraj@neurofin.ai>
2024-08-06 10:09:25 +0530

    testing changes

--- store/types/postLogininput.type.go
+++ store/types/postLogininput.type.go
@@ -12,15 +12,3 @@ type PostLogInput struct {
 	TraceId string 				`json:"traceId"`
 	Timestamp time.Time 		`json:"timestamp"`
 }
-
-func (i *PostLogInput) Validate() error {
-	if err := i.Type.Validate(); err != nil {
-		return err
-	}
-
-	// if i.TraceId == "" {
-	// 	return errors.New("trace id not found")
-	// }
-
-	return nil
-}
